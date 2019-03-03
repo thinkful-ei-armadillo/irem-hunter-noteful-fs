032f159b47770e34cb01178ad5b84757429e8ee0
Irem Sen <secilreel@yahoo.com>
2019-03-03 13:11:28 -0500

    corrected deployment variables for the client

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_ENDPOINT: `https://vast-inlet-99597.herokuapp.com/`,
-    API_KEY: '056da120-2a3a-40db-af63-7b30470a3ae1'
+    API_KEY: process.env.REACT_APP_API_KEY
   }
   
