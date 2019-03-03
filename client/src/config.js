7ed0ea825f77c994ca56f27b3c25315d7b9766a9
Irem Sen <secilreel@yahoo.com>
2019-03-03 12:46:47 -0500

    preparing client for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://vast-inlet-99597.herokuapp.com/`,
     API_KEY: '056da120-2a3a-40db-af63-7b30470a3ae1'
   }
   
